e6d876a90693c477f4c877c9ad8deebe09414284
Musa Issa <musaissa@Musas-MacBook-Air.local>
2022-10-12 22:33:07 +0100

    firebase auth

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-
-import './App.css';
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Login from './firebase.js'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header"></header>
+			<Router>
+				<Switch>
+					<Route exact path="/" component={Login} />
+				</Switch>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
--- src/firebase.js
+++ src/firebase.js
@@ -29,6 +29,7 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 const googleProvider = new GoogleAuthProvider();
+
 const signInWithGoogle = async () => {
 	try {
 		const res = await signInWithPopup(auth, googleProvider);
