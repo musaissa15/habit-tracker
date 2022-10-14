73ba4145b2a0ea4062db08015a3ef2efd4586473
Musa Issa <musaissa@musas-air.home>
2022-10-14 22:36:15 +0100

    finish firebase auth

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import "./App.css";
 import Login from './Login.jsx'
+import Register from "./Register";
+import Reset from "./Reset";
+import Dashboard from "./Dashboard";
 function App() {
 	return (
 		<div className="App">
+			<h1>
+				HII
+			</h1>
 			<header className="App-header"></header>
-			<Router>
+			
 				<Routes>
 					<Route exact path="/" component={Login} />
-					{/* <Route exact path="/register" element={<Register />} />
+					<Route exact path="/register" element={<Register />} />
 					<Route exact path="/reset" element={<Reset />} />
-					<Route exact path="/dashboard" element={<Dashboard />} /> */}
+					<Route exact path="/dashboard" element={<Dashboard />} />
 				</Routes>
-			</Router>
+			
 		</div>
 	);
 }
