66dc0860621d9327316c03df122430c29946604e
Musa Issa <musaissa@musas-air.home>
2022-10-14 22:34:38 +0100

    delete old firebase auth attempt

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import "./App.css";
-import Login from './firebase.js'
+import Login from './Login.jsx'
 function App() {
 	return (
 		<div className="App">
 			<header className="App-header"></header>
 			<Router>
-				<Switch>
+				<Routes>
 					<Route exact path="/" component={Login} />
-				</Switch>
+					{/* <Route exact path="/register" element={<Register />} />
+					<Route exact path="/reset" element={<Reset />} />
+					<Route exact path="/dashboard" element={<Dashboard />} /> */}
+				</Routes>
 			</Router>
 		</div>
 	);
